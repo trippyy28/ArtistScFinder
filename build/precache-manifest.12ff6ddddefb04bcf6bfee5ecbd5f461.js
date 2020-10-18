73cac25a4413b9403c5e571717043cb67ab596eb
trippyy28 <49914713+trippyy28@users.noreply.github.com>
2020-10-18 17:32:08 +0300

    adding gh pages

---
+++ build/precache-manifest.12ff6ddddefb04bcf6bfee5ecbd5f461.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4896820693e9ea5ed17eaa989207c352",
+    "url": "/DjMonsters/index.html"
+  },
+  {
+    "revision": "32e4c7b0fe400581c042",
+    "url": "/DjMonsters/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6c76f80333ff0e377dcf",
+    "url": "/DjMonsters/static/css/main.69a48e77.chunk.css"
+  },
+  {
+    "revision": "32e4c7b0fe400581c042",
+    "url": "/DjMonsters/static/js/2.582064ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DjMonsters/static/js/2.582064ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c76f80333ff0e377dcf",
+    "url": "/DjMonsters/static/js/main.283d40e5.chunk.js"
+  },
+  {
+    "revision": "3a056b31aa5b010e7ad3",
+    "url": "/DjMonsters/static/js/runtime-main.1fbd0b19.js"
+  },
+  {
+    "revision": "091d02bd60ed2c5734614b9473b8b2ad",
+    "url": "/DjMonsters/static/media/scrap-o-holic.091d02bd.woff"
+  }
+]);
